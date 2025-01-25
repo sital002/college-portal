316bca7ccf69945e3f2b9a48edb67a39a7111cb8
sital002 <sitaladhikari002@gmail.com>
2025-01-25 09:41:23 +0545

    update added tailwind css

--- apps/web-app/src/App.tsx
+++ apps/web-app/src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Button } from "@repo/ui/button";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -28,8 +29,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Button appName="test">Hello</Button>
+      <button className="text-red-500">Hello</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
